f3e182aa01a45b25d7dc91796686acdb9d741060
Luma <owner@Owners-MacBook-Air.local>
2020-04-05 02:29:26 -0600

    added jQuery, validate files

---
+++ jQuery.js
@@ -0,0 +1,62 @@
+
+    $(function(){
+        $('#datepicker1').datepicker({
+            dateFormat:'yy/mm/dd',
+            changeMonth:true,
+            changeDay:true,
+            changeYear:true,
+            yearRange:"-75:+0",
+        });
+        $('#datepicker2').datepicker({
+            dateFormat:'yy/mm/dd',
+            changeMonth:true,
+            changeDay:true,
+            changeYear:true,
+            yearRange:"-75:+0",
+        });
+        $('#datepicker3').datepicker({
+            dateFormat:'yy/mm/dd',
+            changeMonth:true,
+            changeDay:true,
+            changeYear:true,
+            yearRange:"-75:+0",
+        });
+        $('#datepicker4').datepicker({
+            dateFormat:'yy/mm/dd',
+            changeMonth:true,
+            changeDay:true,
+            changeYear:true,
+            yearRange:"-75:+0",
+        });
+        $('#datepicker5').datepicker({
+            dateFormat:'yy/mm/dd',
+            changeMonth:true,
+            changeDay:true,
+            changeYear:true,
+            yearRange:"-75:+0",
+        });
+        $('#datepicker6').datepicker({
+            dateFormat:'yy/mm/dd',
+            changeMonth:true,
+            changeDay:true,
+            changeYear:true,
+            yearRange:"-75:+0",
+        });
+        $('#datepicker7').datepicker({
+            dateFormat:'yy/mm/dd',
+            changeMonth:true,
+            changeDay:true,
+            changeYear:true,
+            yearRange:"-75:+0",
+        });
+        $('#datepicker8').datepicker({
+            dateFormat:'yy/mm/dd',
+            changeMonth:true,
+            changeDay:true,
+            changeYear:true,
+            yearRange:"-75:+0",
+        });
+});
+
+
+
